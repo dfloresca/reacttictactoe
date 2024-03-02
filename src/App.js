8d5213f5062210f2e0051ceff8cd72595c3ccc3b
Daniel Floresca <dfloresca@gmail.com>
2024-03-01 20:49:46 -0800

    fix: corrected some punctuation issues

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import {useState } from  'react';
 
 function Square({value, onSquareClick}) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+  return (
+  <button className="square" onClick={onSquareClick}>
+    {value}
+    </button>
+    );
 }
 
 // renders the game board
-function Board(xIsNext, squares, onPlay) {
+function Board({ xIsNext, squares, onPlay }) {
   function handleClick(i) {
     if (calculateWinner(squares) || squares[i]){
       return;
@@ -18,6 +22,7 @@ function Board(xIsNext, squares, onPlay) {
     }
     onPlay(nextSquares)
   } 
+
   const winner = calculateWinner(squares);
   let status;
   if(winner) {
